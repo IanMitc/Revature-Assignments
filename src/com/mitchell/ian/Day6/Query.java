eb5b7b7c25bf122be2ca2e3baf332166e4191662
Ian Mitchell <sweetwaterburns@gmail.com>
2021-10-26 16:09:07 -0600

    Close the scanner

--- src/com/mitchell/ian/Day6/Query.java
+++ src/com/mitchell/ian/Day6/Query.java
@@ -3,27 +3,33 @@ package com.mitchell.ian.Day6;
 import java.util.Scanner;
 
 public class Query {
-    static public int askForInt(String question) throws Exception{
+    static public int askForInt(String question) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print(question + ": ");
-        if(!scanner.hasNextInt())
+        if (!scanner.hasNextInt()) {
+            scanner.close();
             throw new Exception("Please Enter a valid Integer");
+        }
         return scanner.nextInt();
     }
 
     static public double askForDouble(String question) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print(question + ": ");
-        if(!scanner.hasNextDouble())
+        if (!scanner.hasNextDouble()) {
+            scanner.close();
             throw new Exception("Please Enter a valid Number");
+        }
         return scanner.nextDouble();
     }
 
     static public String askForString(String question) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print(question + ": ");
-        if(!scanner.hasNextLine())
+        if (!scanner.hasNextLine()) {
+            scanner.close();
             throw new Exception("Please Enter a valid input");
+        }
         return scanner.nextLine();
     }
 }
